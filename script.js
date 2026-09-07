52201084d4484d5838ed711f1665ae5a5f6ddec6
pikpik-carrot <williammonday2006@gmail.com>
2026-09-06 23:00:08 -0500

    Phase 1

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-console.log("Smart Café JavaScript is connected!");
+// Function declaration
+function calculatePrice(price, quantity) {
+    return price * quantity;
+}
+
+// Function expression
+const getTax = function(amount, taxRate) {
+    return amount * taxRate;
+};
+
+// Console tests
+const subtotal = calculatePrice(15, 4);
+const tax = getTax(subtotal, 0.07);
+
+console.log("Subtotal:", subtotal);
+console.log("Tax:", tax);
