e4738c4dc4bd7bd8cf06565aec2dd48550351aa2
pikpik-carrot <williammonday2006@gmail.com>
2026-09-06 23:02:58 -0500

    Phase 2

--- script.js
+++ script.js
@@ -8,9 +8,23 @@ const getTax = function(amount, taxRate) {
     return amount * taxRate;
 };
 
-// Console tests
+// Phase 1 tests
 const subtotal = calculatePrice(15, 4);
 const tax = getTax(subtotal, 0.07);
 
 console.log("Subtotal:", subtotal);
 console.log("Tax:", tax);
+
+
+// Phase 2
+function calculateStoreTax(amount) {
+    const defaultTaxRate = 0.07;
+
+    return getTax(amount, defaultTaxRate);
+}
+
+// Console test
+console.log("Store Tax:", calculateStoreTax(100));
+
+// This causes an error because defaultTaxRate is local to calculateStoreTax.
+// console.log(defaultTaxRate);
